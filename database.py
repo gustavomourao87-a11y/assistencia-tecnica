e6a02243b9fb8f7b21d49c7c726b9d74ce79824c
Gustavo Teixeira Mourão <gustavomourao87@gmail.com>
2026-09-05 12:11:23 -0300

    feat: cria banco de dados de clientes

---
+++ database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def conectar():
+    conexao = sqlite3.connect("assistencia.db")
+    conexao.row_factory = sqlite3.Row
+    return conexao
+def criar_banco():
+    conexao = conectar()
+    conexao.execute("""
+        CREATE TABLE IF NOT EXISTS clientes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            telefone TEXT NOT NULL,
+            email TEXT,
+            endereco TEXT
+        )
+    """)
+
+    conexao.commit()
+    conexao.close()
+
+if __name__ == "__main__":
+    criar_banco()
+    print("banco de dados criado com sucesso!")
