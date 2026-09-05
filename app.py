5b3758ea58adf71553101e7062a962d8a660fbc7
Gustavo Teixeira Mourão <gustavomourao87@gmail.com>
2026-09-05 11:57:02 -0300

    feat: cria primeira página com Flask

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Sistema de assistencia Técnica")
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
